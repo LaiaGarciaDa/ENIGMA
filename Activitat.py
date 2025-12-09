c57d648e118a048338f2d36defadafe30609ca29
Lena135 <lena.boajram@estudiant.enti.cat>
2025-12-09 13:15:05 +0100

    PROYECTO CREADO

--- Activitat.py
+++
@@ -1,5 +0,0 @@
-from random import randint, random
-
-ABC = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-numrand = randint(0,25)        
-print(ABC[numrand])
